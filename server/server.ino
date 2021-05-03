3ff30875b949a402677470056c5928fe3aa1f63c
Iknowss <chakkarin210300@gmail.com>
2021-05-03 15:50:13 +0700

    import esp8266

--- server/server.ino
+++ server/server.ino
@@ -1,34 +1,23 @@
 /*
+
+ESP8266 NodeMCU Receiver Code (ESP-NOW)
+
 !! main file upload to board !!
 !! flie adjust parameter !!
-Vertion 1.0
+Vertion 2.0
 
 esp32 iot with protocol esp-now
 refre : https://randomnerdtutorials.com/esp-now-many-to-one-esp32/
-ESP-NOW : https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/network/esp_now.html
 
-!! Is receiver or server !!
 The code is prepared to receive data from three
 different boards.
 */
 
 /*
-if your board is esp32 --> include <WiFi.h>
-it's not your board is esp8266 --> include <esp8266WiFi.h>
+Include the ESP8266WiFi and espnow libraries.
 */
-#ifdef ESP32
-  #include <WiFi.h>
-#else
-  #include <ESP8266WiFi.h>
-#endif
-
-/* multi sender data to one receiver vai esp-now protocol */
-/* import library esp-now */
-#include <esp_now.h>
-
-/* import library LCD I2C for show data */
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
 
 /*
 Create structure to recrive date
@@ -53,9 +42,6 @@ message client_3;
 message Client[3] = {
   client_1, client_2, client_3
 };
-
-/* set address LCD finding with find_LCD.ino */
-LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 /* quality board client */
 #define numBoard 3
